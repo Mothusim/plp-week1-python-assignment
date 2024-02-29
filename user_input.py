938e47e4af636734f4e6e9641216a8d92c823ff0
Mothusi Mathuloe <111005114+Mothusim@users.noreply.github.com>
2024-02-29 03:35:34 +0200

    complete solution

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your location: ")
+
+message = 'Hello ' + name + ', your age is ' + age + ' and you are based in ' + location
+
+print(message)
